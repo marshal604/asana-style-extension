47f7914eab8b976346d490246ac33aa990dae964
Hubert <marshal604@gmail.com>
2021-03-28 16:46:26 +0800

    refactor(page): clean code

--- page.js
+++ page.js
@@ -1,17 +1,20 @@
-const init = () => {
-  initCompletedSubtaskStyle();
-};
-
-const initCompletedSubtaskStyle = () => {
-  document.body.classList.add('task-completed-style');
-};
-
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message.checked) {
-    document.body.classList.add(message.id);
-  } else {
-    document.body.classList.remove(message.id);
-  }
-});
-
 init();
+
+function init() {
+  subscribeMessage();
+  initCompletedSubtaskStyle();
+}
+
+function initCompletedSubtaskStyle() {
+  document.body.classList.add('task-completed-style');
+}
+
+function subscribeMessage() {
+  chrome.runtime.onMessage.addListener(function (message) {
+    if (message.checked) {
+      document.body.classList.add(message.id);
+    } else {
+      document.body.classList.remove(message.id);
+    }
+  });
+}
