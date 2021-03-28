5fe2504bd4885715338efe8597afc7b2812559bc
Hubert <marshal604@gmail.com>
2021-03-28 16:45:48 +0800

    refactor: deprecate chrome.tabs.getSelected and use chrome.tabs.query

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
-function init(tab) {
+chrome.tabs.query({ currentWindow: true }, init);
+
+function init([tab]) {
+  subscribeTaskStyleChange(tab);
+}
+
+function subscribeTaskStyleChange(tab) {
   const tabId = tab.id;
   const taskCompletedStyleInput = document.getElementById('ase-task-complete-style-input');
   taskCompletedStyleInput.onchange = function (el) {
+    console.log('tab', tab);
     const checked = el.target.checked;
     chrome.tabs.sendMessage(tabId, {
       id: 'task-completed-style',
@@ -9,5 +16,3 @@ function init(tab) {
     });
   };
 }
-
-chrome.tabs.getSelected(null, init);
